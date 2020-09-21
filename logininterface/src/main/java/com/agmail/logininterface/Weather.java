b86b664e3366bc355994eaf6d3422f7e6faebcb1
Agamil <leichengsigame@gmail.com>
2020-09-21 23:20:01 +0800

    Simple ListView

--- logininterface/src/main/java/com/agmail/logininterface/Weather.java
+++ logininterface/src/main/java/com/agmail/logininterface/Weather.java
@@ -1,6 +1,8 @@
 package com.agmail.logininterface;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,9 @@ public class Weather extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
+        String[] strs = {"1号位", "2号位", "3号位", "4号位", "5号位"};
+        ArrayAdapter<String> myadapter = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, strs);
+        ListView mylistview = findViewById(R.id.listview1);
+        mylistview.setAdapter(myadapter);
     }
 }
